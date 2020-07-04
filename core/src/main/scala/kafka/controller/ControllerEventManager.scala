527b348e1cb8d9b68a59dacf8279c0652ec8af93
yuyang <yuyang.a@mininglamp.com>
2020-07-05 03:16:51 +0800

    config and run test case

---
+++ core/src/main/scala/kafka/controller/ControllerEventManager.scala
@@ -0,0 +1,215 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kafka.controller
+
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.locks.ReentrantLock
+import java.util.concurrent.{CountDownLatch, LinkedBlockingQueue}
+
+import kafka.metrics.{KafkaMetricsGroup, KafkaTimer}
+import kafka.utils.CoreUtils.inLock
+import kafka.utils.ShutdownableThread
+import org.apache.kafka.common.utils.Time
+
+import scala.collection._
+
+/**
+ * 用于保存字符串常量(如线程名称)
+ */
+object ControllerEventManager {
+  val ControllerEventThreadName = "controller-event-thread"
+  val EventQueueTimeMetricName = "EventQueueTimeMs"
+  val EventQueueSizeMetricName = "EventQueueSize"
+}
+
+/**
+ * Controller端事件处理器接口
+ * KafkaController是唯一的实现类
+ */
+trait ControllerEventProcessor {
+  /**
+   * 支持普通处理Controller事件的接口
+   * 接收一个 Controller 事件并进行处理 是实现 Controller 事件处理的主力方法
+   *
+   * @param event
+   */
+  def process(event: ControllerEvent): Unit
+
+  /**
+   * 支持抢占处理Controller事件的接口
+   * 接收一个 Controller 事件 并抢占队列之前的事件进行优先处理 Kafka 使用preempt实现某些高优先级事件的抢占处理
+   * 目前在源码中只有两类事件(ShutdownEventThread 和 Expire)需要抢占式处理
+   *
+   * @param event
+   */
+  def preempt(event: ControllerEvent): Unit
+}
+
+/**
+ * 表示的是事件队列上的事件对象
+ *
+ * @param event         Controller事件
+ * @param enqueueTimeMs Controller事件被放入事件队列的时间戳
+ */
+class QueuedEvent(val event: ControllerEvent, val enqueueTimeMs: Long) {
+  // 标识Controller事件是否开始被处理  QueuedEvent使用CountDownLatch的目的是确保Expire事件在建立ZK会话前被处理 如果不是为了处理Expire事件 使用spent来标识该事件已经被处理过了(如果事件已经被处理过 什么都不做直接返回)
+  val processingStarted = new CountDownLatch(1)
+  // 标识Controller事件是否被处理过
+  val spent = new AtomicBoolean(false)
+
+  /**
+   * 处理Controller事件
+   *
+   * @param processor
+   */
+  def process(processor: ControllerEventProcessor): Unit = {
+    if (spent.getAndSet(true))
+      return
+    processingStarted.countDown()
+    processor.process(event)
+  }
+
+  /**
+   * 抢占式处理Controller事件
+   *
+   * @param processor
+   */
+  def preempt(processor: ControllerEventProcessor): Unit = {
+    if (spent.getAndSet(true))
+      return
+    processor.preempt(event)
+  }
+
+  /**
+   * 阻塞等待事件处理完成
+   */
+  def awaitProcessing(): Unit = {
+    processingStarted.await()
+  }
+
+  override def toString: String = {
+    s"QueuedEvent(event=$event, enqueueTimeMs=$enqueueTimeMs)"
+  }
+}
+
+/**
+ * Controller事件处理器 用于创建和管理 ControllerEventThread(事件处理线程) 和 事件队列
+ *
+ * @param controllerId
+ * @param processor
+ * @param time
+ * @param rateAndTimeMetrics
+ */
+class ControllerEventManager(controllerId: Int,
+                             processor: ControllerEventProcessor,
+                             time: Time,
+                             rateAndTimeMetrics: Map[ControllerState, KafkaTimer]) extends KafkaMetricsGroup {
+
+  import ControllerEventManager._
+
+  @volatile private var _state: ControllerState = ControllerState.Idle
+  private val putLock = new ReentrantLock()
+  private val queue = new LinkedBlockingQueue[QueuedEvent]
+  // Visible for test
+  private[controller] val thread = new ControllerEventThread(ControllerEventThreadName)
+
+  private val eventQueueTimeHist = newHistogram(EventQueueTimeMetricName)
+
+  newGauge(EventQueueSizeMetricName, () => queue.size)
+
+  def state: ControllerState = _state
+
+  def start(): Unit = thread.start()
+
+  def close(): Unit = {
+    try {
+      thread.initiateShutdown()
+      clearAndPut(ShutdownEventThread)
+      thread.awaitShutdown()
+    } finally {
+      removeMetric(EventQueueTimeMetricName)
+      removeMetric(EventQueueSizeMetricName)
+    }
+  }
+
+  /**
+   * 向事件队列中写入Controller事件
+   *
+   * @param event
+   * @return
+   */
+  def put(event: ControllerEvent): QueuedEvent = inLock(putLock) {
+    val queuedEvent = new QueuedEvent(event, time.milliseconds())
+    queue.put(queuedEvent)
+    queuedEvent
+  }
+
+  /**
+   * 向事件队列中写入Controller事件 和put不同 该方法会先执行高优先级的抢占事件 之后清空事件队列 最后向事件队列中写入事件
+   *
+   * @param event
+   * @return
+   */
+  def clearAndPut(event: ControllerEvent): QueuedEvent = inLock(putLock) {
+    queue.forEach(_.preempt(processor))
+    queue.clear()
+    put(event)
+  }
+
+  def isEmpty: Boolean = queue.isEmpty
+
+  /**
+   * 专属的事件处理线程 唯一的作用是处理不同种类的ControllerEvent 从事件队列中读取Controller事件
+   * ShutdownableThread是Kafka为很多线程类定义的公共父类 其父类是是 Java Thread 类
+   *
+   * @param name 由ControllerEventManager对象定义的
+   */
+  class ControllerEventThread(name: String) extends ShutdownableThread(name = name, isInterruptible = false) {
+    logIdent = s"[ControllerEventThread controllerId=$controllerId] "
+
+    override def doWork(): Unit = {
+      // 从事件队列中获取Controller事件 如果事件队列为空 将一直阻塞
+      val dequeued = queue.take()
+      dequeued.event match {
+        // 当ControllerEventManager关闭时会向事件队列中写入ShutdownEventThread事件显示通知ControllerEventThread线程关闭 此处什么也不需要做 因为关闭ControllerEventThread的逻辑是由外部调用的
+        case ShutdownEventThread => // The shutting down of the thread has been initiated at this point. Ignore this event.
+        // 其他事件调用process方法进行处理
+        case controllerEvent =>
+          _state = controllerEvent.state
+          // 更新对应的事件在事件队列中保存的时间
+          eventQueueTimeHist.update(time.milliseconds() - dequeued.enqueueTimeMs)
+
+          try {
+            def process(): Unit = dequeued.process(processor) // 调用QueueEvent的process方法
+            // 处理Controller事件 同时计算处理速率
+            rateAndTimeMetrics.get(state) match {
+              case Some(timer) => timer.time {
+                process()
+              }
+              case None => process() // 该方法首先调用QueueEvent的process方法判断是否已经被处理过 如果被处理过直接返回 如果没被处理调用ControllerEventProcessor的process方法进行处理
+            }
+          } catch {
+            case e: Throwable => error(s"Uncaught error processing event $controllerEvent", e)
+          }
+
+          _state = ControllerState.Idle
+      }
+    }
+  }
+
+}
---
+++ core/src/main/scala/kafka/log/OffsetIndex.scala
@@ -0,0 +1,256 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kafka.log
+
+import java.io.File
+import java.nio.ByteBuffer
+
+import kafka.utils.CoreUtils.inLock
+import kafka.utils.Logging
+import org.apache.kafka.common.errors.InvalidOffsetException
+
+/**
+ * An index that maps offsets to physical file locations for a particular log segment. This index may be sparse:
+ * that is it may not hold an entry for all messages in the log.
+ *
+ * The index is stored in a file that is pre-allocated to hold a fixed maximum number of 8-byte entries.
+ *
+ * The index supports lookups against a memory-map of this file. These lookups are done using a simple binary search variant
+ * to locate the offset/location pair for the greatest offset less than or equal to the target offset.
+ *
+ * Index files can be opened in two ways: either as an empty, mutable index that allows appends or
+ * an immutable read-only index file that has previously been populated. The makeReadOnly method will turn a mutable file into an
+ * immutable one and truncate off any extra bytes. This is done when the index file is rolled over.
+ *
+ * No attempt is made to checksum the contents of this file, in the event of a crash it is rebuilt.
+ *
+ * The file format is a series of entries. The physical format is a 4 byte "relative" offset and a 4 byte file location for the
+ * message with that offset. The offset stored is relative to the base offset of the index file. So, for example,
+ * if the base offset was 50, then the offset 55 would be stored as 5. Using relative offsets in this way let's us use
+ * only 4 bytes for the offset.
+ *
+ * The frequency of entries is up to the user of this class.
+ *
+ * All external APIs translate from relative offsets to full offsets, so users of this class do not interact with the internal
+ * storage format.
+ */
+// Avoid shadowing mutable `file` in AbstractIndex
+// 定义位移索引 保存<位移值, 文件磁盘物理位置>
+class OffsetIndex(_file: File, baseOffset: Long, maxIndexSize: Int = -1, writable: Boolean = true)
+  extends AbstractIndex(_file, baseOffset, maxIndexSize, writable) {
+
+  import OffsetIndex._
+
+  /**
+   * 位移值用4字节表示(虽然使用了Long类型 但是该位移值是相对于起始位移值baseOffset而言的 -> [真实位移值 - 起始位移值] 所以只占用4字节 节省了磁盘空间) 物理磁盘位置也用4字节表示
+   * broker端参数log.segment.bytes是整型 即Kafka每个日志段文件的大小不会超过 2 ^ 32(4GB)
+   * 说明同一个日志段文件上的位移值 - baseOffset的差值一定在整数范围内 所以保存4字节就足够了
+   *
+   *
+   * @return
+   */
+  override def entrySize = 8
+
+  /* the last offset in the index */
+  private[this] var _lastOffset = lastEntry.offset
+
+  debug(s"Loaded index file ${file.getAbsolutePath} with maxEntries = $maxEntries, " +
+    s"maxIndexSize = $maxIndexSize, entries = ${_entries}, lastOffset = ${_lastOffset}, file position = ${mmap.position()}")
+
+  /**
+   * The last entry in the index
+   */
+  private def lastEntry: OffsetPosition = {
+    inLock(lock) {
+      _entries match {
+        case 0 => OffsetPosition(baseOffset, 0)
+        case s => parseEntry(mmap, s - 1)
+      }
+    }
+  }
+
+  def lastOffset: Long = _lastOffset
+
+  /**
+   * Find the largest offset less than or equal to the given targetOffset
+   * and return a pair holding this offset and its corresponding physical file position.
+   *
+   * 定位消息所在的物理磁盘位置
+   *
+   * @param targetOffset The offset to look up. 目标位移值参数
+   * @return The offset found and the corresponding file position for this offset
+   *         If the target offset is smaller than the least entry in the index (or the index is empty),
+   *         the pair (baseOffset, 0) is returned.
+   */
+  def lookup(targetOffset: Long): OffsetPosition = {
+    maybeLock(lock) {
+      val idx = mmap.duplicate // 复制mmap 共享索引文件的内容
+      val slot = largestLowerBoundSlotFor(idx, targetOffset, IndexSearchType.KEY) // 内部调用二分查找找到索引项对象所在的槽
+      if (slot == -1)
+        OffsetPosition(baseOffset, 0) // 没有找到返回一个空的槽 即物理文件从头开始读
+      else
+        parseEntry(idx, slot) // 返回slot槽对应的索引项  即不大于给定位移值 targetOffset 的最大位移值
+    }
+  }
+
+  /**
+   * Find an upper bound offset for the given fetch starting position and size. This is an offset which
+   * is guaranteed to be outside the fetched range, but note that it will not generally be the smallest
+   * such offset.
+   */
+  def fetchUpperBoundOffset(fetchOffset: OffsetPosition, fetchSize: Int): Option[OffsetPosition] = {
+    maybeLock(lock) {
+      val idx = mmap.duplicate
+      val slot = smallestUpperBoundSlotFor(idx, fetchOffset.position + fetchSize, IndexSearchType.VALUE)
+      if (slot == -1)
+        None
+      else
+        Some(parseEntry(idx, slot))
+    }
+  }
+
+  /**
+   * 计算相对位移值
+   *
+   * @param buffer
+   * @param n
+   * @return
+   */
+  private def relativeOffset(buffer: ByteBuffer, n: Int): Int = buffer.getInt(n * entrySize)
+
+  /**
+   * 计算物理位移值
+   *
+   * @param buffer
+   * @param n
+   * @return
+   */
+  private def physical(buffer: ByteBuffer, n: Int): Int = buffer.getInt(n * entrySize + 4)
+
+  /**
+   * {@link IndexEntry} 的实现
+   * baseOffset + relativeOffset(buffer, n) 用于还原完整位移值
+   * physical(buffer, n)计算出消息在日志度文件中的物理位置
+   * 将绝对位移值和物理位移值存入
+   *
+   * @param buffer the buffer of this memory mapped index.
+   * @param n      the slot. 查找给定ByteBuffer中保存的第n个索引项(第n个槽)
+   * @return the index entry stored in the given slot.
+   */
+  override protected def parseEntry(buffer: ByteBuffer, n: Int): OffsetPosition = {
+    OffsetPosition(baseOffset + relativeOffset(buffer, n), physical(buffer, n))
+  }
+
+  /**
+   * Get the nth offset mapping from the index
+   *
+   * @param n The entry number in the index
+   * @return The offset/position pair at that entry
+   */
+  def entry(n: Int): OffsetPosition = {
+    maybeLock(lock) {
+      if (n >= _entries)
+        throw new IllegalArgumentException(s"Attempt to fetch the ${n}th entry from index ${file.getAbsolutePath}, " +
+          s"which has size ${_entries}.")
+      parseEntry(mmap, n)
+    }
+  }
+
+  /**
+   * Append an entry for the given offset/location pair to the index. This entry must have a larger offset than all subsequent entries.
+   *
+   * 向索引文件中写入新的索引项
+   *
+   * @throws InvalidOffsetException if the offset causes index offset to overflow
+   */
+  def append(offset: Long, position: Int): Unit = {
+    inLock(lock) { // 索引对象的各个属性可能被多个线程并发修改 需要加锁保证线程安全
+      // 1.判断日志文件是否已经写满
+      require(!isFull, "Attempt to append to a full index (size = " + _entries + ").")
+      // 2.Kafka不允许写入一个比最新索引项还小的索引项 所以必须满足以下两个条件才允许写入:
+      // ----a.当前索引文件为空
+      // ----b.待写入的索引项位移值 > 当前以写入的索引项位移值(Kafka规定索引项中的位移值必须是单调增加的)
+      if (_entries == 0 || offset > _lastOffset) {
+        trace(s"Adding index entry $offset => $position to ${file.getAbsolutePath}")
+        mmap.putInt(relativeOffset(offset)) // 3A.向mmap写入相对位移值
+        mmap.putInt(position) // 3B.向mmap写入物理位移值
+        // 4. 更新元数据信息 即索引项计数和当前索引项最新位移
+        _entries += 1
+        _lastOffset = offset
+        // 5.检验写入的的索引项格式是否满足要求 即索引项个数 * 单个索引项占用字节数 == 当前文件物理大小 不想等说明文件已损坏
+        require(_entries * entrySize == mmap.position(), s"$entries entries but file position in index is ${mmap.position()}.")
+      } else {
+        // 无法写入时抛出的异常
+        throw new InvalidOffsetException(s"Attempt to append an offset ($offset) to position $entries no larger than" +
+          s" the last offset appended (${_lastOffset}) to ${file.getAbsolutePath}.")
+      }
+    }
+  }
+
+  override def truncate() = truncateToEntries(0)
+
+  override def truncateTo(offset: Long): Unit = {
+    inLock(lock) {
+      val idx = mmap.duplicate
+      val slot = largestLowerBoundSlotFor(idx, offset, IndexSearchType.KEY)
+
+      /* There are 3 cases for choosing the new size
+       * 1) if there is no entry in the index <= the offset, delete everything
+       * 2) if there is an entry for this exact offset, delete it and everything larger than it
+       * 3) if there is no entry for this offset, delete everything larger than the next smallest
+       */
+      val newEntries =
+        if (slot < 0)
+          0 // 截断全部
+        else if (relativeOffset(idx, slot) == offset - baseOffset)
+          slot // 截断该消息及之后的内容
+        else
+          slot + 1 // 截断比下一个消息的位移大的所有消息
+      truncateToEntries(newEntries)
+    }
+  }
+
+  /**
+   * Truncates index to a known number of entries.
+   *
+   * @param entries 表示截取到哪个槽
+   */
+  private def truncateToEntries(entries: Int): Unit = {
+    inLock(lock) {
+      _entries = entries // 更新索引项计数
+      mmap.position(_entries * entrySize) // 表示要截断到位置
+      _lastOffset = lastEntry.offset // 更新索引文件的最大位移值
+      debug(s"Truncated index ${file.getAbsolutePath} to $entries entries;" +
+        s" position is now ${mmap.position()} and last offset is now ${_lastOffset}")
+    }
+  }
+
+  override def sanityCheck(): Unit = {
+    if (_entries != 0 && _lastOffset < baseOffset)
+      throw new CorruptIndexException(s"Corrupt index found, index file (${file.getAbsolutePath}) has non-zero size " +
+        s"but the last offset is ${_lastOffset} which is less than the base offset $baseOffset.")
+    if (length % entrySize != 0)
+      throw new CorruptIndexException(s"Index file ${file.getAbsolutePath} is corrupt, found $length bytes which is " +
+        s"neither positive nor a multiple of $entrySize.")
+  }
+
+}
+
+object OffsetIndex extends Logging {
+  override val loggerName: String = classOf[OffsetIndex].getName
+}
